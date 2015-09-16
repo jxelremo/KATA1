57cf35c5210975497d2f73044853663a03725ecd
usuario <usuario@10.22.143.34>
2015-09-16 09:43:59 +0100

    Segunda version

--- src/proyecto1/Person.java
+++ src/proyecto1/Person.java
@@ -14,6 +14,7 @@ public class Person {
     private final String name;
     private final String surname;
     private final Date birthday;
+    private static final long MILLISECOND_PER_YEAR = (long) (1000*60*60*24*365.25);
 
     public Person(String name, String surname, Date birthday) {
         this.name = name;
@@ -39,6 +40,10 @@ public class Person {
     
     public int getAge(){
         Date today = new Date(); 
-        return (int)((today.getTime()-birthday.getTime())/(1000*60*60*24*365.25));
+        return (int)(getMillis(today.getTime()-birthday.getTime()));
+    }
+    
+    private long getMillis(long milli){
+        return milli/MILLISECOND_PER_YEAR;
     }
 }
